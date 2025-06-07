4d7d2d4f96574e938eba4f57d901b20a2101cf2a
Zack <72500445+lilrebel17@users.noreply.github.com>
2025-06-07 17:30:10 -0400

    7 feature request normalize return data for commands (#9)
    * Added output_json.py to standardize outputs
    
    * Added output_json to disable_user.py
    
    * Added output_json to enable_user.py
    
    * Fixed command name to use arg.command
    
    * Added output_json to get_mailbox
    
    * Added indent to output_json
    
    * Added output_json to set_password
    
    * Updated version number to 0.1.3
    
    * Updated text if requests dont return 200
    
    * Added @ between email & domain in subcommands output_json call
    
    * Updated the text field for set_passwords output
    
    * Updated Version to 0.2.0

--- src/rackmail/subcommands/disable_user.py
+++ src/rackmail/subcommands/disable_user.py
@@ -1,4 +1,5 @@
 from ..client import RackspaceClient
+from ..utils.output_json import output_json
 from requests import put
 
 def disable_user(args):
@@ -7,8 +8,10 @@ def disable_user(args):
     url = f"https://api.emailsrvr.com/v1/customers/{rackspace_client.customer_id}/domains/{args.domain}/rs/mailboxes/{args.email}"
     request = put(url,headers=RackspaceClient().auth_header,json={"enabled":False})
 
-    if request.status_code == 200:
-        print("Rackspace API returned 200, account was disabled.")
-    else:
-        print(request.text)
-        print(f"Status Code: {request.status_code}")
+    print(
+        output_json(
+            request.status_code,
+            args.command,
+            f"{args.email}@{args.domain}",
+            "User was disabled" if request.status_code == 200 else request.text)
+        )
