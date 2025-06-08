977dd189c6536fb3491fcbad747e9ed02989b713
Zack <72500445+lilrebel17@users.noreply.github.com>
2025-06-08 00:07:03 -0400

    V0.2.0
    * 7 feature request normalize return data for commands (#9)
    
    * Added output_json.py to standardize outputs
    
    * Added output_json to disable_user.py
    
    * Added output_json to enable_user.py
    
    * Fixed command name to use arg.command
    
    * Added output_json to get_mailbox
    
    * Added indent to output_json
    
    * Added output_json to set_password
    
    * Updated version number to 0.1.3
    
    * Updated text if requests dont return 200
    
    * Added @ between email & domain in subcommands output_json call
    
    * Updated the text field for set_passwords output
    
    * Updated Version to 0.2.0
    
    * Create pypi_push.yml (#10)
    
    Testing automated publishing with github actions
    
    * Update pypi_push.yml for pypi
    
    Tested this action on testpypi, these changes should move it to the real pypi website.

--- src/rackmail/subcommands/set_password.py
+++ src/rackmail/subcommands/set_password.py
@@ -1,5 +1,6 @@
 from ..client import RackspaceClient
 from ..utils.generate_random_string import generate_random_string
+from ..utils.output_json import output_json
 from requests import put
 
 def set_password(args):
@@ -13,8 +14,10 @@ def set_password(args):
     url = f"https://api.emailsrvr.com/v1/customers/{rackspace_client.customer_id}/domains/{args.domain}/rs/mailboxes/{args.email}"
     request = put(url,headers=RackspaceClient().auth_header,json={"password":password})
 
-    if request.status_code == 200:
-        print(f"Rackspace API returned 200, password for {args.email}{args.domain} changed to {password}")
-    else:
-        print(request.text)
-        print(f"Status Code: {request.status_code}")
+    print(
+        output_json(
+            request.status_code,
+            args.command,
+            f"{args.email}@{args.domain}",
+            {"Password":password} if request.status_code == 200 else request.text)
+        )
