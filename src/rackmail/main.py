7e1195bada252c93eeb43c1c9ca68307837e69a8
Zack Farmer <zackfarmer1710@gmail.com>
2025-06-16 00:24:47 -0400

    Added set subcommand

--- src/rackmail/main.py
+++ src/rackmail/main.py
@@ -3,10 +3,8 @@ from .version import VERSION
 from .subcommands.enable_user import enable_user
 from .subcommands.disable_user import disable_user
 from .subcommands.get_mailbox import get_mailbox
+from .subcommands.set_property import set_property
 from .subcommands.set_password import set_password
-
-def debug_print(args):
-    print(args)
 
 def main():
     main_parser = argparse.ArgumentParser(prog="rackmail",description="CLI to interact with Rackspace's Hosted Email API",)
@@ -27,10 +25,29 @@ def main():
     get_mailbox_subcommand = subparsers.add_parser("getmailbox",description="Gets all available information about a mailbox",parents=[global_command_parser])
     get_mailbox_subcommand.set_defaults(func=get_mailbox)
 
-    change_password_subcommand = subparsers.add_parser("set",description="Sets a property on a mailbox",parents=[global_command_parser])
-    change_password_subcommand.add_argument("property",action="store",metavar="property",help="The property being changed")
-    change_password_subcommand.add_argument("value",action="store",metavar="value",help="The updated value of the property being changed")
-    change_password_subcommand.set_defaults(func=debug_print)
+    change_password_subcommand = subparsers.add_parser("setpassword",description="Sets the password of a mailbox",parents=[global_command_parser])
+    change_password_subcommand.add_argument("-p","--password",action="store",metavar="password",dest="password",help="The password you want set on a mailbox",required=False,default=None)
+    change_password_subcommand.set_defaults(func=set_password)
+
+    set_subcommand = subparsers.add_parser(
+        "set",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=(
+            "Set Rackspace API properties that don’t need their own command.\n"
+            "Use this for things like BusinessUnit, TimeZone, or other one-off fields.\n\n"
+            "Examples:\n"
+            "  rackmail set BusinessUnit WaffleOps -e user.name -d mydomain.com\n"
+            "  rackmail set TimeZone America/New_York -e user.name -d mydomain.com\n"
+        ),
+        epilog=(
+            "For a list of all possible properties visit RackSpace Hosted Email API Reference\n"
+            "http://api-wiki.apps.rackspace.com/api-wiki/index.php/Rackspace_Mailbox_(Rest_API)\n\n"
+            "Note: This command does not validate property names or values—use it when you know what you’re doing."
+        ),
+        parents=[global_command_parser])
+    set_subcommand.add_argument("property",action="store",metavar="property",help="The property being changed")
+    set_subcommand.add_argument("value",action="store",metavar="value",help="The updated value of the property being changed")
+    set_subcommand.set_defaults(func=set_property)
     
     args = main_parser.parse_args()
 
